e791e540a2f03292a585570467e22dc6e6d2b8d1
Abicg <30852646+Abicg@users.noreply.github.com>
2017-08-18 19:13:55 +0200

    Add files via upload

---
+++ sensordemovimiento.ino
@@ -0,0 +1,20 @@
+const int sensorpin=A5;//
+const int ledpin=13;
+
+
+void setup() {
+Serial.begin(9600);
+pinMode(sensorpin, OUTPUT);
+pinMode(ledpin, OUTPUT);
+
+}
+ 
+void loop() {
+
+int SensorStatus=analogRead(sensorpin);
+Serial.print(SensorStatus);
+Serial.print("\n");
+  digitalWrite(ledpin,SensorStatus);
+ 
+delay(100);
+}
