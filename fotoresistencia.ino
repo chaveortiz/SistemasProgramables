93d7e0d372a6d424ebfb9e517628651d5c56cc73
chaveortiz <30836119+chaveortiz@users.noreply.github.com>
2017-08-18 19:10:58 +0200

    Add files via upload

---
+++ fotoresistencia.ino
@@ -0,0 +1,22 @@
+const int ledpin=13;//
+const int ldrpin=A0;
+
+
+void setup() {
+Serial.begin(9600);
+pinMode(ledpin, OUTPUT);
+pinMode(ldrpin, INPUT);
+
+}
+
+void loop() {
+
+int ldrStatus=analogRead(ldrpin);
+Serial.print(ldrStatus);
+Serial.print("\n");
+if(ldrStatus>100)
+  digitalWrite(ledpin,HIGH);
+  else
+  digitalWrite(ledpin,LOW);
+delay(100);
+}
