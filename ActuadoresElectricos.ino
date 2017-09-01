5c5301bf913233cf3677a002608baf22a04cefd7
BarbaraBL <30819112+BarbaraBL@users.noreply.github.com>
2017-09-01 03:41:57 +0200

    Add files via upload

---
+++ ActuadoresElectricos.ino
@@ -0,0 +1,21 @@
+const int pinSensor=3;
+const int pinTransitor=4;
+int estado;
+void setup() {
+  // put your setup code here, to run once:
+
+
+pinMode(pinSensor,INPUT);
+pinMode(pinTransitor,OUTPUT);
+digitalWrite(pinTransitor,LOW);
+}
+void loop() {
+  // put your main code here, to run repeatedly:
+estado=digitalRead (pinSensor);
+if(estado ==HIGH){
+  digitalWrite(pinTransitor, HIGH);
+}
+else{
+  digitalWrite(pinTransitor,LOW);
+}
+}
