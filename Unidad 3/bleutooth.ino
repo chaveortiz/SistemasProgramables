2f953d3aae21844febba7b086f3ac11d847b2fce
chaveortiz <30836119+chaveortiz@users.noreply.github.com>
2017-09-30 22:26:16 -0500

    Add files via upload

---
+++ Unidad 3/bleutooth.ino
@@ -0,0 +1,15 @@
+#include <SoftwareSerial.h> // Incluimos la librería SoftwareSerial 
+SoftwareSerial BT(10,11); // Definimos los pines RX y TX del Arduino conectados al Bluetooth
+
+void setup(){ Serial.begin(9600); // Inicializamos el puerto serie 
+BT.begin(38400); // Inicializamos el puerto serie BT (Para Modo AT 2) 
+Serial.println("Esperando comando "); }
+
+void loop() { 
+  if(BT.available()) // Si llega un dato por el puerto BT se envía al monitor serial 
+  { Serial.write(BT.read()); }
+
+if(Serial.available()) // Si llega un dato por el monitor serial se envía al puerto BT 
+{ BT.write(Serial.read()); }
+
+}
