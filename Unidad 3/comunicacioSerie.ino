fe15b4c57cfeec8a901c5f4e8b23025bc30c6bb3
chaveortiz <30836119+chaveortiz@users.noreply.github.com>
2017-09-30 21:31:25 -0500

    Add files via upload

---
+++ Unidad 3/comunicacioSerie.ino
@@ -0,0 +1,36 @@
+boolean v1=false; 
+boolean v2=false;
+
+int numero1=0; 
+int numero2=0;
+
+void setup() {
+  Serial.begin(9600);
+  delay(1000); 
+  Serial.print("Ingrese dos numeros");
+
+}
+
+void loop() { 
+  if(v1 && v2){
+    Serial.print("\nLa suma es "); 
+    Serial.print(numero1+numero2); 
+    v1=false; v2=false; 
+    }
+    } 
+    void serialEvent(){
+      while(Serial.available()){
+        if(!v1){ 
+          numero1=Serial.readString().toInt(); 
+          Serial.print("\nPrimer numero:"); 
+          Serial.print(numero1); v1=true; 
+          }
+          else{ 
+            if(!v2){
+              numero2=Serial.readString().toInt();
+              Serial.print("\nSegundo numero:"); 
+              Serial.print(numero2); v2=true; 
+              } 
+           } 
+       } 
+   }
